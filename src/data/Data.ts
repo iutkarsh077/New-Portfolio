14667901a7032ca72ba41f62f42f2ec0041a05ee
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-29 15:50:06 +0530

    sequence changed

--- src/data/Data.ts
+++ src/data/Data.ts
@@ -5,12 +5,12 @@ const data = [
     { src: "/assets/MysteryBox.png", alt: "MysteryBox Project Screenshot", redirectedTo: "https://mystery-box-omega.vercel.app/" },
     { src: "/assets/port-netflix.png", alt: "Netflix Project Screenshot", redirectedTo: "https://videostreaming-phi-black.vercel.app/" },
     { src: "/assets/coderCourses.png", alt: "Coding Courses Platform Screenshot", redirectedTo: "https://coding-courses.vercel.app/"},
-    { src: "/assets/healthify.png", alt: "Healthify App Screenshot", redirectedTo: "https://healthify-lake.vercel.app/" },
-    { src: "/assets/shopsmart.png", alt: "ShopSmart Project Screenshot", redirectedTo: "https://shopsmart-green.vercel.app/" },
-    { src: "/assets/RealTales.png", alt: "RealTales Project Screenshot", redirectedTo: "https://real-tales.vercel.app/" },
     {
       src: "/assets/taskmaster.png", alt: "Taskmaster", redirectedTo: "https://task-master-coral.vercel.app/"
     },
+    { src: "/assets/healthify.png", alt: "Healthify App Screenshot", redirectedTo: "https://healthify-lake.vercel.app/" },
+    { src: "/assets/RealTales.png", alt: "RealTales Project Screenshot", redirectedTo: "https://real-tales.vercel.app/" },
+    { src: "/assets/shopsmart.png", alt: "ShopSmart Project Screenshot", redirectedTo: "https://shopsmart-green.vercel.app/" },
     { src: "/assets/Text to speech - Google Chrome 19-10-2023 13_40_52.png", alt: "Text to Speech Project Screenshot", redirectedTo: "https://iutkarsh077.github.io/Text-To-Speech/" },
     { src: "/assets/signup.png", alt: "Authentication Page Screenshot", redirectedTo: "https://auth-seven-chi.vercel.app/signup" },
     { src: "/assets/Text to speech - Google Chrome 19-10-2023 13_43_25.png", alt: "Weather App Screenshot", redirectedTo: "https://iutkarsh077.github.io/Weather-App/" },
